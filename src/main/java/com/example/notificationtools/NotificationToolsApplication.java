aaac306e897f5e95a51a3f5a3f5fb3d272116b5d
xia0ne <1223605525@qq.com>
2023-12-05 14:50:18 +0800

    init: 初始化项目

---
+++ src/main/java/com/example/notificationtools/NotificationToolsApplication.java
@@ -0,0 +1,13 @@
+package com.example.notificationtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/notificationtools/NotificationToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notificationtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
